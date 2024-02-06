5f313c33809389f9490cf4f95f59a04a8dd4b809
Roman Burlaka <burlakarv@gmail.com>
2024-02-06 19:11:09 +0100

    testBannerPage

--- src/test/java/automation/page_object_model/HomePage.java
+++ src/test/java/automation/page_object_model/HomePage.java
@@ -3,6 +3,8 @@ package automation.page_object_model;
 
 import automation.base.BasePage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 
 public class HomePage extends BasePage {
@@ -13,6 +15,14 @@ public class HomePage extends BasePage {
 
         return getDriver().getTitle();
 
+    }
+
+    @FindBy(className = "banner-image")
+    WebElement banner;
+
+
+    public Boolean bannerIsVisible() {
+        return true ? banner.isDisplayed(): false;
     }
 }
 
